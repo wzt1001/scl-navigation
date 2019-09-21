040c6b4dc667334fa4e7c65ceb060c26bc61f072
Zhoutong Wang <wzt1001@hotmail.com>
2019-09-21 17:03:46 -0400

    sep 21

--- agent.py
+++ agent.py
@@ -9,14 +9,14 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 1          # minibatch size
+BUFFER_SIZE = 3         # replay buffer size, default int(1e5)
+BATCH_SIZE = 5          # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+UPDATE_EVERY = 8        # how often to update the network
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 #device = torch.device("cpu")
 
 class Agent():
